d5cd91664899a5380cdf2c5c28ece6534e76b101
evanMarshallTheBay <evan.marshall@thebay.com>
2022-07-15 14:00:58 -0300

    first commit

---
+++ notes/js/math/math.js
@@ -0,0 +1,10 @@
+console.log('Hello');
+console.log(Math.PI);
+console.log(Math.floor(23.933));
+console.log(Math.E);
+
+// Generate random numbers.
+console.log(Math.random());
+
+// Generate random numbers between 1 and 10.
+console.log(Math.floor(Math.random() * 10) + 1); // Trailing 1 makes it so that it has a 1 minimum.
